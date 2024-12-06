dffe0c4e6bd76e6c34b593bc50de7c56075db7ad
Thrishul <Jeedipalli.Reddy@coforge.com>
2024-12-06 14:56:57 +0530

    angular producthive-consumer product CURD operations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { CreateProductsComponent } from './components/create-products/create-products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'products',component:ProductComponent},
+    {path:'add-product/:pid',component:CreateProductsComponent},
+    {path:'product-details/:pid',component:ProductDetailsComponent}
+];
---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { ProductService } from '../../services/product.service';
+import { Router } from '@angular/router';
+import { Product } from '../../model/product';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+
+  products:Observable<Product[]>|any;
+  message:string='';
+
+  constructor(private productService:ProductService,private router:Router){}
+
+  ngOnInit():void{ //angular life cycle method-invokes when component is initialized
+    this.reloadData();
+  }
+
+  reloadData(){
+    this.productService.getProductList().subscribe({
+      next:(data)=>{
+        this.products=data;
+      },
+      error:(err)=>{
+        console.error('error fetching Product List: '+err.message);
+      }
+    });
+  }
+  addProduct():void{
+    this.router.navigate(['/add-product/_add'])
+  }
+
+  productDetails(pid:Number):void{
+    this.router.navigate(['/product-details',pid]);
+  }
+
+  editProduct(pid:Number):void{
+    this.router.navigate(['/add-product',pid])
+  }
+
+  deleteProduct(pid:number):void{
+    this.productService.deleteProduct(pid).subscribe({
+      next: () => {
+        this.message = 'Product deleted successfully.';
+        setTimeout(() => {
+          this.message = '';
+          this.reloadData();  // Refresh products list after deletion
+        }, 2000);  // Clear the message after 2 seconds
+      },
+      error: (err) => {
+        console.error('Error deleting product:', err.message);  // Handle the error
+        this.message = 'Error deleting product. Please try again later.';
+      }
+    });
+  }
+}
